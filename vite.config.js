717857e97bdf0d9fc94e3fff8f21809e92f9d591
thinkerdust <56123956+thinkerdust@users.noreply.github.com>
2026-01-16 21:53:10 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/js/script.js',
+                'resources/js/scripts.js',
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/pages/management/company/index.js',
